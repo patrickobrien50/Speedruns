53109810f0f5fd5753a8801e3d0f773fe50d1e7d
smashbro6894 <patrickobrien831@gmail.com>
2017-09-25 19:33:11 -0700

    Commented out methods that weren't doing anything.

--- SpeedrunAPITEsts/CustomGamesTableViewCell.swift
+++ SpeedrunAPITEsts/CustomGamesTableViewCell.swift
@@ -14,15 +14,15 @@ class CustomGamesTableViewCell: UITableViewCell {
     @IBOutlet weak var gameNameLabel: UILabel!
     
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 
 }
